2b638ac30e17e1b894ff2a4ea5698ae737a20ae3
kahyaoglou <furkan.kahyaoglu.3@gmail.com>
2022-04-27 01:52:11 +0300

    Tasarımsal değişiklikler yapıldı

--- YazılımYapımıProje/Form1.Designer.cs
+++ YazılımYapımıProje/Form1.Designer.cs
@@ -35,52 +35,61 @@
             this.txtGirisKullaniciAdi = new System.Windows.Forms.TextBox();
             this.txtGirisSifre = new System.Windows.Forms.TextBox();
             this.btnKayıtOl = new System.Windows.Forms.Button();
-            this.btnGiris = new System.Windows.Forms.Button();
-            this.lblSifeyiUnuttum = new System.Windows.Forms.Label();
             this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnGiris = new System.Windows.Forms.Button();
             this.ımageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.lblSifeyiUnuttum = new System.Windows.Forms.Label();
+            this.ımageList3 = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
             // 
             // lblKullaniciAdiGiris
             // 
             this.lblKullaniciAdiGiris.AllowDrop = true;
             this.lblKullaniciAdiGiris.AutoSize = true;
-            this.lblKullaniciAdiGiris.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblKullaniciAdiGiris.Location = new System.Drawing.Point(142, 125);
+            this.lblKullaniciAdiGiris.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblKullaniciAdiGiris.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblKullaniciAdiGiris.ImageKey = "user icon.ico";
+            this.lblKullaniciAdiGiris.ImageList = this.ımageList2;
+            this.lblKullaniciAdiGiris.Location = new System.Drawing.Point(56, 117);
             this.lblKullaniciAdiGiris.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblKullaniciAdiGiris.Name = "lblKullaniciAdiGiris";
-            this.lblKullaniciAdiGiris.Size = new System.Drawing.Size(98, 21);
+            this.lblKullaniciAdiGiris.Size = new System.Drawing.Size(185, 32);
             this.lblKullaniciAdiGiris.TabIndex = 0;
-            this.lblKullaniciAdiGiris.Text = "Kullanıcı Adı:";
+            this.lblKullaniciAdiGiris.Text = "     Kullanıcı Adı:";
+            this.lblKullaniciAdiGiris.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.lblKullaniciAdiGiris.Click += new System.EventHandler(this.lblKullaniciAdiGiris_Click);
             // 
             // lblSifreGiris
             // 
             this.lblSifreGiris.AutoSize = true;
-            this.lblSifreGiris.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblSifreGiris.Location = new System.Drawing.Point(195, 156);
+            this.lblSifreGiris.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSifreGiris.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblSifreGiris.ImageKey = "password icn.ico";
+            this.lblSifreGiris.ImageList = this.ımageList3;
+            this.lblSifreGiris.Location = new System.Drawing.Point(139, 162);
             this.lblSifreGiris.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSifreGiris.Name = "lblSifreGiris";
-            this.lblSifreGiris.Size = new System.Drawing.Size(45, 21);
+            this.lblSifreGiris.Size = new System.Drawing.Size(102, 32);
             this.lblSifreGiris.TabIndex = 0;
-            this.lblSifreGiris.Text = "Şifre:";
+            this.lblSifreGiris.Text = "     Şifre:";
+            this.lblSifreGiris.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.lblSifreGiris.Click += new System.EventHandler(this.lblSifreGiris_Click);
             // 
             // txtGirisKullaniciAdi
             // 
-            this.txtGirisKullaniciAdi.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtGirisKullaniciAdi.Location = new System.Drawing.Point(247, 125);
+            this.txtGirisKullaniciAdi.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtGirisKullaniciAdi.Location = new System.Drawing.Point(248, 114);
             this.txtGirisKullaniciAdi.Name = "txtGirisKullaniciAdi";
-            this.txtGirisKullaniciAdi.Size = new System.Drawing.Size(216, 25);
+            this.txtGirisKullaniciAdi.Size = new System.Drawing.Size(216, 39);
             this.txtGirisKullaniciAdi.TabIndex = 1;
             this.txtGirisKullaniciAdi.TextChanged += new System.EventHandler(this.txtGirisKullaniciAdi_TextChanged);
             // 
             // txtGirisSifre
             // 
-            this.txtGirisSifre.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtGirisSifre.Location = new System.Drawing.Point(247, 156);
+            this.txtGirisSifre.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtGirisSifre.Location = new System.Drawing.Point(248, 159);
             this.txtGirisSifre.Name = "txtGirisSifre";
-            this.txtGirisSifre.Size = new System.Drawing.Size(216, 25);
+            this.txtGirisSifre.Size = new System.Drawing.Size(216, 39);
             this.txtGirisSifre.TabIndex = 1;
             this.txtGirisSifre.TextChanged += new System.EventHandler(this.txtGirisSifre_TextChanged);
             // 
@@ -89,7 +98,7 @@
             this.btnKayıtOl.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnKayıtOl.ImageKey = "ne user ico.ico";
             this.btnKayıtOl.ImageList = this.ımageList1;
-            this.btnKayıtOl.Location = new System.Drawing.Point(247, 187);
+            this.btnKayıtOl.Location = new System.Drawing.Point(248, 204);
             this.btnKayıtOl.Name = "btnKayıtOl";
             this.btnKayıtOl.Size = new System.Drawing.Size(105, 36);
             this.btnKayıtOl.TabIndex = 2;
@@ -98,12 +107,19 @@
             this.btnKayıtOl.UseVisualStyleBackColor = true;
             this.btnKayıtOl.Click += new System.EventHandler(this.btnKatıtOl_Click);
             // 
+            // ımageList1
+            // 
+            this.ımageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "ne user ico.ico");
+            // 
             // btnGiris
             // 
             this.btnGiris.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnGiris.ImageKey = "user icon.ico";
             this.btnGiris.ImageList = this.ımageList2;
-            this.btnGiris.Location = new System.Drawing.Point(358, 187);
+            this.btnGiris.Location = new System.Drawing.Point(359, 204);
             this.btnGiris.Name = "btnGiris";
             this.btnGiris.Size = new System.Drawing.Size(105, 36);
             this.btnGiris.TabIndex = 2;
@@ -112,23 +128,6 @@
             this.btnGiris.UseVisualStyleBackColor = true;
             this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
             // 
-            // lblSifeyiUnuttum
-            // 
-            this.lblSifeyiUnuttum.AutoSize = true;
-            this.lblSifeyiUnuttum.Location = new System.Drawing.Point(288, 226);
-            this.lblSifeyiUnuttum.Name = "lblSifeyiUnuttum";
-            this.lblSifeyiUnuttum.Size = new System.Drawing.Size(120, 21);
-            this.lblSifeyiUnuttum.TabIndex = 3;
-            this.lblSifeyiUnuttum.Text = "Şifreyi Unuttum";
-            this.lblSifeyiUnuttum.Click += new System.EventHandler(this.lblSifeyiUnuttum_Click);
-            // 
-            // ımageList1
-            // 
-            this.ımageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
-            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
-            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
-            this.ımageList1.Images.SetKeyName(0, "ne user ico.ico");
-            // 
             // ımageList2
             // 
             this.ımageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
@@ -136,10 +135,28 @@
             this.ımageList2.TransparentColor = System.Drawing.Color.Transparent;
             this.ımageList2.Images.SetKeyName(0, "user icon.ico");
             // 
+            // lblSifeyiUnuttum
+            // 
+            this.lblSifeyiUnuttum.AutoSize = true;
+            this.lblSifeyiUnuttum.Location = new System.Drawing.Point(291, 243);
+            this.lblSifeyiUnuttum.Name = "lblSifeyiUnuttum";
+            this.lblSifeyiUnuttum.Size = new System.Drawing.Size(120, 21);
+            this.lblSifeyiUnuttum.TabIndex = 3;
+            this.lblSifeyiUnuttum.Text = "Şifreyi Unuttum";
+            this.lblSifeyiUnuttum.Click += new System.EventHandler(this.lblSifeyiUnuttum_Click);
+            // 
+            // ımageList3
+            // 
+            this.ımageList3.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList3.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList3.ImageStream")));
+            this.ımageList3.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList3.Images.SetKeyName(0, "password icn.ico");
+            // 
             // FrmGiris
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(584, 411);
             this.Controls.Add(this.lblSifeyiUnuttum);
             this.Controls.Add(this.btnGiris);
@@ -152,7 +169,7 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmGiris";
             this.RightToLeftLayout = true;
-            this.Text = "ANA SAYFA";
+            this.Text = "Ana Sayfa";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -170,5 +187,6 @@
         private Label lblSifeyiUnuttum;
         private ImageList ımageList1;
         private ImageList ımageList2;
+        private ImageList ımageList3;
     }
 }
